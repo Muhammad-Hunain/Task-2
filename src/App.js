1e41eb0b3bc37ff5285b4f6e98df8e1174c1d1d2
Muhammad-Hunian <Hunain29.mem@gmail.com>
2021-10-30 20:10:05 +0500

    First-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React ,{useState} from 'react'; 
 function App() {
+  const [users,setUsers] = useState([]);
+  const loadUsersData  = async()=>{
+  const responseData = await fetch ("https://api.github.com/users")
+  const jsonResponse = await responseData.json(); 
+    setUsers(jsonResponse)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="heading-1">
+      <i class="fab fa-affiliatetheme"></i>DATA FETCH    
+   </h1>
+      <button class="button" onClick={loadUsersData}>Get Data</button>   
+      <h2 class="heading-2"> USERS DATA:</h2>
+      <ul id ="data">
+      {users.map(({id,login,type}) =>(
+          <>
+          <li key = {id}>Name :{login} <br/> Type : {type}</li>
+          
+          </>  
+        ))}
+      </ul>
     </div>
   );
 }
